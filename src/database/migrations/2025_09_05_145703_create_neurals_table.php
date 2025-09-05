e2454161237ac480494feeb4272d6f10ff2c69cc
aiten_laptop <uuum1984@mail.ru>
2025-09-05 20:44:19 +0300

    add phpmyadmin and fix orchid

--- src/database/migrations/2025_09_05_145703_create_neurals_table.php
+++ src/database/migrations/2025_09_05_145703_create_neurals_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('neurals', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name', 40);
+            $table->string('link', 150);
         });
     }
 
